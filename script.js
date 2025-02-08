063dd61803ac5c5b2f57115f1d0c0004675bf5a7
deltabladex <121876767+deltabladex@users.noreply.github.com>
2025-02-08 12:37:56 +1100

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function checkAnswer() {
     const errorMessage = document.getElementById("error-message");
     
     if (answer === "reshu") {
-        window.location.href = "https://your-destination-site.com"; // Replace with your actual URL
+        window.location.href = "https://deltabladex.github.io/val2025/"; // Replace with your actual URL
     } else {
         errorMessage.textContent = "Wrong answer! Try again.";
     }
